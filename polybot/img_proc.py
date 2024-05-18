3b98b7b28e919e4c5cbfab8bad0a9d798c6abb07
ghalebb <ghaleb.butto99@gmail.com>
2024-05-18 11:17:06 +0300

    img class methods

--- polybot/img_proc.py
+++ polybot/img_proc.py
@@ -1,3 +1,4 @@
+import random
 from pathlib import Path
 from matplotlib.image import imread, imsave
 
@@ -46,22 +47,37 @@ class Img:
         for i, row in enumerate(self.data):
             res = []
             for j in range(1, len(row)):
-                res.append(abs(row[j-1] - row[j]))
+                res.append(abs(row[j - 1] - row[j]))
 
             self.data[i] = res
 
     def rotate(self):
-        # TODO remove the `raise` below, and write your implementation
-        raise NotImplementedError()
+        self.data = [list(row) for row in zip(*self.data[::-1])]
 
     def salt_n_pepper(self):
-        # TODO remove the `raise` below, and write your implementation
-        raise NotImplementedError()
+
+        for row in range(len(self.data)):
+            for col in range(len(self.data[0])):
+                random_number = random.random()
+                if random_number < 0.2:
+                    self.data[row][col] = 255
+                elif random_number > 0.8:
+                    self.data[row][col] = 0
 
     def concat(self, other_img, direction='horizontal'):
-        # TODO remove the `raise` below, and write your implementation
-        raise NotImplementedError()
+        if direction == 'horizontal':
+            if len(self.data) != len(other_img.data):
+                raise RuntimeError("Images must have the same height for horizontal concatenation.")
+            self.data = [self_row + other_row for self_row, other_row in zip(self.data, other_img.data)]
+        elif direction == 'vertical':
+            if len(self.data[0] != len(other_img.data[0])):
+                raise RuntimeError("Images must have the same width for vertical concatenation.")
+            self.data.extend(other_img.data)
+        else:
+            raise ValueError("Invalid direction. Use 'horizontal' or 'vertical'.")
 
     def segment(self):
-        # TODO remove the `raise` below, and write your implementation
-        raise NotImplementedError()
+        for i in range(len(self.data)):
+            for j in range(len(self.data[0])):
+                self.data[i][j] = 255 if self.data[i][j] > 100 else 0
+        # self.data = [[255 if pixel > 100 else 0 for pixel in row] for row in self.data]

2abca812227d88dfd52213181b8d132be73d7ce2
Ubuntu <ghaleb.butto99@gmail.com>
2024-06-02 22:39:08 +0000

    add greeting feature for 'hi' message

--- polybot/app.py
+++ polybot/app.py
@@ -9,6 +9,17 @@ app = flask.Flask(__name__)
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 TELEGRAM_APP_URL = os.environ['TELEGRAM_APP_URL']
 
+@app.route('/{}'.format(TELEGRAM_TOKEN), methods=['POST'])
+def respond():
+    update = telegram.Update.de_json(request.get_json(force=True), bot)
+
+    chat_id = update.message.chat.id
+    text = update.message.text
+
+    if text.lower() == "hi":
+        bot.sendMessage(chat_id=chat_id, text="Hello! How can I assist you today?")
+
+    return 'ok'
 
 @app.route('/', methods=['GET'])
 def index():
